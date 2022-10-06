410b02fa348acfdd19d176a6a08b10341019c3bd
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-10-06 14:09:28 +0300

    describes issues with current implementation $ python3 hello.py IndexError: list index out of range
    $ python3 hello.py James John
    Hello there James

--- hello.py
+++ hello.py
@@ -1,11 +1,15 @@
-'''
-take some command line arguements
-'''
-
 import sys
 
 
 def main():
+    '''
+    - takes a command line arguments
+    - problems:
+        1. If no argmuent is specified
+            IndexError: list index out of range
+        2. If more than one argument is passed
+            Will consider only the first arg 
+    '''
     print(f'Hello there {sys.argv[1]}')
 
 

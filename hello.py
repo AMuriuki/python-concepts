99326f7fc1d6f2ce1285ab3cd2967aec039e4e45
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-10-06 14:32:34 +0300

    handle when arguments are passed manually to the main function $ python3 >>> import hello >>> hello.main(['Arnold']) Hello there Arnold

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
 import argparse
 
 
-def main():
+def main(argv=None):
     '''
-    - uses `argparse` argument parser to exceptions 
+    - uses `argparse` argument parser to handle exceptions 
     - from previous issues
     '''
     parser = argparse.ArgumentParser()
     # defines one positional argument named person
     parser.add_argument('person')
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
     print(f'Hello there {args.person}')
 
 
